761f93edd3aa5af2ad56389a0f184af5b97dc598
Farhan Khwaja <farhan.khwaja@gmail.com>
2016-04-10 11:44:50 -0700

    Reviews Cralwed, Extracted and pushed

--- crawler.sh
+++ crawler.sh
@@ -1,6 +1,6 @@
 input_file=$1
 
-mkdir review_data
+# mkdir review_data
 
 while read line; do 
 	url="$(echo $line | cut -d' ' -f1)"
@@ -8,9 +8,9 @@ while read line; do
 	output_file="$(echo $line |cut -d' ' -f2)"
 	
 	curl "${url}" > "review_data/${output_file}.txt"
-	curl "${url}?start=40" >> "review_data/data/${output_file}.txt"
-	curl "${url}?start=80" >> "review_data/data/${output_file}.txt"
-	curl "${url}?start=120" >> "review_data/data/${output_file}.txt"
-	curl "${url}?start=160" >> "review_data/${output_file}.txt"
+	curl "${url}?start=20" >> "review_data/${output_file}.txt"
+	curl "${url}?start=40" >> "review_data/${output_file}.txt"
+	curl "${url}?start=60" >> "review_data/${output_file}.txt"
+	curl "${url}?start=80" >> "review_data/${output_file}.txt"
 	
 done < $input_file
--- extract_reviews.py
+++ extract_reviews.py
@@ -5,37 +5,42 @@ def crawlData(sourceDir, destDir):
 
     html_tags = re.compile(r'<[^>]+>')
     for file in sourceFiles:
-        filepath = sourceDir+file;
-        inFile = open(filepath,'r')
+        filepath = sourceDir+'/'+file;
 
-        data = ""
-        result = ""
-        rating = 0
-        goodReview = False
-        for line in inFile:
-            foundreview = line.find('<i class="star-img',0)
+        if file.endswith(".txt"):
+            inFile = open(filepath,'r')
+            print(inFile)
+            data = ""
+            result = ""
+            rating = 0
+            goodReview = False
+            for line in inFile:
+                foundreview = line.find('<i class="star-img',0)
 
-            if foundreview > -1:
-                found4review = line.find('title="4.0 star rating">')
-                found45review = line.find('title="4.5 star rating">')
-                found5review = line.find('title="5.0 star rating">')
-                if (found5review > -1) or (found4review > -1) or (found45review > -1):
-                    goodReview = True
-                else:
+                if foundreview > -1:
+                    found25review = line.find('title="2.5 star rating">')
+                    found3review = line.find('title="3.0 star rating">')
+                    found35review = line.find('title="3.5 star rating">')
+                    found4review = line.find('title="4.0 star rating">')
+                    found45review = line.find('title="4.5 star rating">')
+                    found5review = line.find('title="5.0 star rating">')
+                    if (found5review > -1) or (found4review > -1) or (found45review > -1) or (found35review > -1) or (found3review > -1) or (found25review > -1):
+                        goodReview = True
+                    else:
+                        goodReview = False
+
+                foundreview = line.find('<p itemprop="description" lang="en">',0)
+
+                if foundreview > -1:
+                    if goodReview:
+                        result = result+' '+html_tags.sub(' ',line)
+                        
                     goodReview = False
 
-            foundreview = line.find('<p itemprop="description" lang="en">',0)
-
-            if foundreview > -1:
-                if goodReview:
-                    result = result+' '+html_tags.sub(' ',line)
-                    
-                goodReview = False
-
-        outFile = open(destDir+file,'w')
-        outFile.write(result)
-        outFile.close()
-        inFile.close()
+            outFile = open(destDir+'/'+file,'w')
+            outFile.write(result)
+            outFile.close()
+            inFile.close()
 
 if __name__ == "__main__":
     crawlData(str(sys.argv[1]),str(sys.argv[2]))
